2e6d481b6811f2d26cc37b81f764e762eeb10eed
Elperry <Mohammedelpry@yahoo.com>
2016-12-09 15:59:18 +0200

    start

--- msg.php
+++ msg.php
@@ -4,7 +4,7 @@
 </head>
 <body id="container">
 <center>
-<div id="msg"><h1>Success Registration , You will be redirected within 5 Second</h1>
+<div id="msg" style="background: #123150;"><h1><br /><br /><br />Success Registration , You will be redirected within 5 Second</h1>
 <?php header( "refresh:5;url=/hms/index.php" );?> </div>
  </center>
 </body>
